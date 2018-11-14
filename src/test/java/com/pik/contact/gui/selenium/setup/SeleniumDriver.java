7e744ff26170e1009bad4527bbe96901347b4e34
Dileep Gudivada <41387498+dileep2018@users.noreply.github.com>
2018-11-14 13:30:11 +0530

    Update SeleniumDriver.java

--- src/test/java/com/pik/contact/gui/selenium/setup/SeleniumDriver.java
+++ src/test/java/com/pik/contact/gui/selenium/setup/SeleniumDriver.java
@@ -7,11 +7,9 @@ import java.util.concurrent.TimeUnit;
 
 public class SeleniumDriver {
 
-    //static private WebDriver driver;
+    
     public static void main(String[] args) {
            	System.setProperty("webdriver.firefox.marionette",/usr/local/bin/geckodriver");
-    //public static WebDriver getDriver() {
-        //if (driver == null) {
             WebDriver driver = new FirefoxDriver();    //can be replaced with HtmlUnitDriver for better performance
             driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
             driver.get("http://54.145.226.14:8081/contact/");
