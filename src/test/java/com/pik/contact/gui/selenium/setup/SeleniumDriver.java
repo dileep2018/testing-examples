e74cc7fe162856c3a659c2d77c0d91b6eb6f114d
Dileep Gudivada <41387498+dileep2018@users.noreply.github.com>
2018-11-14 17:58:51 +0530

    Update SeleniumDriver.java

--- src/test/java/com/pik/contact/gui/selenium/setup/SeleniumDriver.java
+++ src/test/java/com/pik/contact/gui/selenium/setup/SeleniumDriver.java
@@ -12,8 +12,6 @@ public class SeleniumDriver {
     public static WebDriver getDriver() {
         if (driver == null) {
             File pathToBinary = new File("/home/ubuntu/.mozilla/firefox/s7hoae7c.default");
-            FirefoxBinary firefoxBinary = new FirefoxBinary(pathBinary);
-            System.setProperty("webdriver.gecko.driver","/usr/local/bin/geckodriver");
             driver = new FirefoxDriver();    //can be replaced with HtmlUnitDriver for better performance
             driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
         }
