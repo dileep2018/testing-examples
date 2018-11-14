7ffea1eff3f641f13322de95882cb513077c175e
Dileep Gudivada <41387498+dileep2018@users.noreply.github.com>
2018-11-14 11:09:47 +0530

    Update SeleniumDriver.java

--- src/test/java/com/pik/contact/gui/selenium/setup/SeleniumDriver.java
+++ src/test/java/com/pik/contact/gui/selenium/setup/SeleniumDriver.java
@@ -1,8 +1,7 @@
 package com.pik.contact.gui.selenium.setup;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.util.concurrent.TimeUnit;
 
@@ -12,12 +11,14 @@ public class SeleniumDriver {
 
     public static WebDriver getDriver() {
         if (driver == null) {
-            System.setProperty("webdriver.chrome.driver","/usr/local/bin/chromedriver");
-            ChromeOptions Options= new ChromeOptions();
-            WebDriver driver= new ChromeDriver(Options);   //can be replaced with HtmlUnitDriver for better performance
+            System.setProperty("webdriver.gecko.driver", "/usr/local/bin/geckodriver");
+            driver = new FirefoxDriver();    //can be replaced with HtmlUnitDriver for better performance
+            FirefoxDriver driver = new FirefoxDriver();
+            driver.get("http://54.145.226.14:8081/contact/");
             driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
         }
         return driver;
     }
 
 }
+
