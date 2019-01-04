766d2edacfb0df89401b12986344ce5266298ff0
Dileep Gudivada <41387498+dileep2018@users.noreply.github.com>
2019-01-04 15:56:23 +0530

    Update SeleniumDriver.java

--- src/test/java/com/pik/contact/gui/selenium/setup/SeleniumDriver.java
+++ src/test/java/com/pik/contact/gui/selenium/setup/SeleniumDriver.java
@@ -19,7 +19,7 @@ public class SeleniumDriver {
       // if (driver == null) {
             driver = new ChromeDriver();    
             driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
-           return driver;
-           driver.quit();
+         return driver;
+       //driver.quit();
      }
 }
