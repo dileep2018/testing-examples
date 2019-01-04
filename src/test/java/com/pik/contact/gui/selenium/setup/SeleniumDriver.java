affafff4e91942e2c012df013ba0fa8110e4ac82
Dileep Gudivada <41387498+dileep2018@users.noreply.github.com>
2019-01-04 11:54:48 +0530

    Update SeleniumDriver.java

--- src/test/java/com/pik/contact/gui/selenium/setup/SeleniumDriver.java
+++ src/test/java/com/pik/contact/gui/selenium/setup/SeleniumDriver.java
@@ -1,6 +1,6 @@
- package com.pik.contact.gui.selenium.setup;
-
+package com.pik.contact.gui.selenium.setup;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxDriverLogLevel;
 import org.openqa.selenium.firefox.FirefoxOptions;
@@ -12,8 +12,9 @@ public class SeleniumDriver {
     static private WebDriver driver;
 
     public static WebDriver getDriver() {
-        if (driver == null) {
             driver = new FirefoxDriver();    //can be replaced with HtmlUnitDriver for better performance
+            driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+            driver = new ChromeDriver();    //can be replaced with HtmlUnitDriver for better performance
             driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
         }
         return driver;
