c66bd84faea2b21edd2cc898f5d0b3bbdf6ce954
Dileep Gudivada <41387498+dileep2018@users.noreply.github.com>
2019-01-04 14:40:12 +0530

    Update SeleniumDriver.java

--- src/test/java/com/pik/contact/gui/selenium/setup/SeleniumDriver.java
+++ src/test/java/com/pik/contact/gui/selenium/setup/SeleniumDriver.java
@@ -16,7 +16,6 @@ public class SeleniumDriver {
             driver = new FirefoxDriver();    //can be replaced with HtmlUnitDriver for better performance
             driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
         //}
-         return driver;
       // if (driver == null) {
             driver = new ChromeDriver();    //can be replaced with HtmlUnitDriver for better performance
             driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
