0525ae96f5816858aba2d98cf66e7d3514c2222c
Dileep Gudivada <41387498+dileep2018@users.noreply.github.com>
2019-01-04 11:12:32 +0530

    Update SeleniumDriver.java

--- src/test/java/com/pik/contact/gui/selenium/setup/SeleniumDriver.java
+++ src/test/java/com/pik/contact/gui/selenium/setup/SeleniumDriver.java
@@ -1,6 +1,7 @@
 package com.pik.contact.gui.selenium.setup;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxDriverLogLevel;
 import org.openqa.selenium.firefox.FirefoxOptions;
